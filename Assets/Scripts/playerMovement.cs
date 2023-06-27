750560500fef2a2814e5ba675f11436435f7fed4
CassaX <matheuscassatti@estudante.ufscar.br>
2023-06-27 14:11:38 -0300

    Criação do Flip e Inicio Arco
    Atualizei o script de movimentação com a função flip no qual quando o personagem se movimenta para uma direção o corpo e direção segue junto, e iniciei os scripts do arco e flecha, falta atualizar eles para ter mira.

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -6,6 +6,7 @@ public class playerMovement : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float walkSpeed = 5;
+    bool facingRight = true;
     void Start()
     {
         
@@ -16,11 +17,28 @@ public class playerMovement : MonoBehaviour
         // Andar para direita
         if(Input.GetKey(KeyCode.D)) {
             myRigidbody.velocity = Vector2.right * walkSpeed;
+            
+            if(!facingRight){
+                Flip();
+            }
         }
         
         // Andar para esquerda
         if(Input.GetKey(KeyCode.A)) {
             myRigidbody.velocity = Vector2.left * walkSpeed;
+            
+            if(facingRight){
+                Flip();
+            }
         }
+
+
+    }
+
+    void Flip(){
+        
+        facingRight = !facingRight;
+        transform.Rotate(0,180,0);
+
     }
 }
