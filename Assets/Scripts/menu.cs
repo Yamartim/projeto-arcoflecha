6e5dd2e2127b4c9ba86d4f34487c0092641945e9
yararodri <137322018+yararodri@users.noreply.github.com>
2023-06-28 13:38:03 -0300

    Menu
    Iniciando menu

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuprincipal : MonoBehaviour {
+    public void sair (){
+        Debug.Log("sair");
+        Application.Quit();
+    }
+
+    //public void jogar (){
+    //    SceneManagement.LoadScene(1);
+    //}
+
+}
