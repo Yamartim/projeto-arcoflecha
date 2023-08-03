d6dc6451bc850686509bbe3dc0266b7abef79ec1
João Vitor Simplicio <joaovitorvs0309@gmail.com>
2023-08-02 22:10:30 -0300

    erro de nome de arquivo

--- Assets/Scripts/efeitosSonoros.cs
+++ Assets/Scripts/efeitosSonoros.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class efeitosSonoros : MonoBehaviour
+public class EfeitosSonoros : MonoBehaviour
 {
     public AudioSource src;
     public AudioClip atirarFlecha, coletarFlecha, coletarAnel;
