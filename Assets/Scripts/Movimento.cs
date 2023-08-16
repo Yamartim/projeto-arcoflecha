dd113ba97283fbccd8680688bb0c2fef07dca905
Yamartim <54243954+Yamartim@users.noreply.github.com>
2023-08-16 02:29:01 -0300

    corda funcionandooo (quase)
    tipo uns 80% ela ta bugada pra subir mas de resto ta ótima

--- Assets/Scripts/Movimento.cs
+++ Assets/Scripts/Movimento.cs
@@ -20,16 +20,17 @@ public class Movimento : MonoBehaviour
     float inputHorizontal, inputVertical;
 
     bool cordaProxima, escalando;
-    public float velEscalada = 20;
+    public float velEscalada = 1f;
     
-    //HingeJoint2D hj;
-    //Rigidbody2D cordaProxRB;
+    HingeJoint2D hj;
+    Rigidbody2D cordaProxRB;
+    GameObject cordaAtual = null;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<CapsuleCollider2D>();
-        //hj = GetComponent<HingeJoint2D>();
+        hj = GetComponent<HingeJoint2D>();
 
         mat = new PhysicsMaterial2D("Material");
         mat.friction = 0.4f;
@@ -38,44 +39,103 @@ public class Movimento : MonoBehaviour
 
     void Update()
     {
-
         inputHorizontal = Input.GetAxisRaw("Horizontal");
         inputVertical = Input.GetAxisRaw("Vertical");
 
-        // Andar
 
-        bool podeAndarDir = inputHorizontal == 1 && rb.velocity.x < velMax;
-        bool podeAndarEsq = inputHorizontal == -1 && rb.velocity.x > -velMax;
-        bool podeAndar = podeAndarDir || podeAndarEsq;
-        if(inputHorizontal != 0 && podeAndar) {
-            rb.velocity += Vector2.right * inputHorizontal * aceleracao * Time.deltaTime;
-            mat.friction = 0f;
-            col.sharedMaterial = mat;
-        } else if(inputHorizontal == 0) {
-            // Se não estiver andando, adicionar atrito
-            mat.friction = 0.4f;
-            col.sharedMaterial = mat;
-        }
-        
+        // Andar
+        InputAndar();
+
         // Pular
-        isGrounded = Physics2D.OverlapCapsule(groundCheck.position, 
-            new Vector2(0, 0.3f), CapsuleDirection2D.Horizontal, 
-            0, groundLayer);
-        if(Input.GetButtonDown("Jump") && (isGrounded || escalando)) {
-            rb.velocity = new Vector2(rb.velocity.x, forcaPulo);
-            escalando = false;
-        }
+        InputPulo();
 
         InputEscalada();
     }
 
+    private void InputAndar()
+    {
+        bool podeAndarDir = inputHorizontal == 1 && rb.velocity.x < velMax;
+        bool podeAndarEsq = inputHorizontal == -1 && rb.velocity.x > -velMax;
+        bool podeAndar = podeAndarDir || podeAndarEsq;
+        if (inputHorizontal != 0 && podeAndar)
+        {
+            rb.velocity += Vector2.right * inputHorizontal * aceleracao * Time.deltaTime;
+            mat.friction = 0f;
+            col.sharedMaterial = mat;
+        }
+        else if (inputHorizontal == 0)
+        {
+            // Se não estiver andando, adicionar atrito
+            mat.friction = 0.4f;
+            col.sharedMaterial = mat;
+        }
+    }
+
+    private void InputPulo()
+    {
+        isGrounded = Physics2D.OverlapCapsule(groundCheck.position,
+                    new Vector2(0, 0.3f), CapsuleDirection2D.Horizontal,
+                    0, groundLayer);
+        if (Input.GetButtonDown("Jump") && (isGrounded || escalando))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, forcaPulo);
+            SoltarCorda();
+        }
+    }
+
+    
+    void InputEscalada()
+    {
+
+        if(cordaProxima && (Mathf.Abs(inputVertical) > 0))
+        {
+            AgarrarCorda(cordaProxRB);
+        }
+
+        if(escalando)
+        {
+            SegmentoCorda seg = hj.connectedBody.GetComponent<SegmentoCorda>();
+
+            rb.gravityScale = 0f;
+            //rb.velocity = new Vector2(rb.velocity.x, inputVertical * velMax);
+            hj.connectedAnchor += Vector2.up * inputVertical * Time.deltaTime *velEscalada;
+
+            if (hj.connectedAnchor.y > 0f)
+            {
+                if (seg.EhSegmentoInicial()){
+                    SoltarCorda();
+                } else {
+                    Debug.Log("sobe um segmento...");
+                    hj.connectedAnchor = new Vector2(0, hj.connectedAnchor.y % -1);
+                    hj.connectedBody = seg.conectadoAcima.GetComponent<Rigidbody2D>();
+                }
+            }
+
+            if (hj.connectedAnchor.y < -1f)
+            {
+                if (seg.EhSegmentoFinal()){
+                    SoltarCorda();
+                } else {
+                    Debug.Log("desce um segmento...");
+                    hj.connectedAnchor = new Vector2(0, hj.connectedAnchor.y % -1);
+                    hj.connectedBody = seg.conectadoAbaixo.GetComponent<Rigidbody2D>();
+                }
+            }
+            
+        }else{
+            rb.gravityScale = 3f;
+
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other) 
     {
-        if(other.CompareTag("corda"))
+        if(other.CompareTag("corda") && !cordaProxima)
         {
             cordaProxima = true;
-            //cordaProxRB = other.gameObject.GetComponent<Rigidbody2D>();
+            cordaProxRB = other.gameObject.GetComponent<Rigidbody2D>();
         }
+
     }
 
     private void OnTriggerStay2D(Collider2D other) 
@@ -83,7 +143,7 @@ public class Movimento : MonoBehaviour
         if(other.CompareTag("corda"))
         {
             cordaProxima = true;
-            //cordaProxRB = other.gameObject.GetComponent<Rigidbody2D>();
+            cordaProxRB = other.gameObject.GetComponent<Rigidbody2D>();
         }
     }
 
@@ -93,33 +153,30 @@ public class Movimento : MonoBehaviour
         if(other.CompareTag("corda"))
         {
             cordaProxima = false;
-            escalando = false;
-            //cordaProxRB = null;
+            cordaProxRB = null;
         }
     }
 
-    void InputEscalada()
+    void AgarrarCorda(Rigidbody2D corda)
     {
-
-        if(cordaProxima && (Mathf.Abs(inputVertical) > 0))
+        if (!escalando && corda.transform.parent != cordaAtual)
         {
             escalando = true;
-            //hj.enabled = true;
-            //hj.connectedBody = cordaProxRB;
+            hj.enabled = true;
+            hj.connectedBody = corda;
+            cordaAtual = corda.transform.parent.gameObject;
+            Debug.Log("Segurei corda! " + hj.connectedAnchor);
         }
-
-        if(escalando)
-        {
-            rb.gravityScale = 0f;
-            rb.velocity = new Vector2(rb.velocity.x, inputVertical * velMax);
             
-            mat.friction = 0.4f;
-            col.sharedMaterial = mat;
-
-        }else{
-            rb.gravityScale = 3f;
-            //hj.enabled = false;
-
-        }
     }
+
+    void SoltarCorda()
+    {
+        Debug.Log("Soltei corda! " + hj.connectedAnchor);
+        escalando = false;
+        hj.enabled = false;
+        hj.connectedBody = null;
+        cordaAtual = null;
+    }
+
 }
--- Assets/Scripts/SegmentoCorda.cs
+++ Assets/Scripts/SegmentoCorda.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class SegmentoCorda : MonoBehaviour
 {
-
-    GameObject conectadoAcima, conectadoAbaixo;
+    public GameObject conectadoAcima, conectadoAbaixo;
     HingeJoint2D hj;
 
 
@@ -14,6 +13,7 @@ public class SegmentoCorda : MonoBehaviour
     {
         hj = GetComponent<HingeJoint2D>();
         conectadoAcima = hj.connectedBody.gameObject;
+        conectadoAbaixo = null;
         PosicionarSegmento();
     }
 
@@ -23,11 +23,22 @@ public class SegmentoCorda : MonoBehaviour
         if(segAcima != null)
         {
             segAcima.conectadoAbaixo = this.gameObject;
-            float pontoConexao = conectadoAcima.GetComponent<SpriteRenderer>().bounds.size.y;
+            //float pontoConexao = conectadoAcima.GetComponent<SpriteRenderer>().bounds.size.y;
+            float pontoConexao = 1f; //ta zuado de fazer a função dinamica então fica assim msm
             
             hj.connectedAnchor = new Vector2(0, pontoConexao*-1);
         } else {
             hj.connectedAnchor = Vector2.zero;
         }
+    }
+
+    public bool EhSegmentoInicial()
+    {
+        return conectadoAcima.GetComponent<SegmentoCorda>() == null;
+    }
+
+    public bool EhSegmentoFinal()
+    {
+        return conectadoAbaixo == null;
     }
 }
