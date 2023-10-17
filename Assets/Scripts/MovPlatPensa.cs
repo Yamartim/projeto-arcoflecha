33a16be740193d157f7da3c7cac30e1de98f09e8
pedmig <pedromiguel.sp17@gmail.com>
2023-10-17 14:07:22 -0300

    plataforma suspensa tá pronta

--- Assets/Scripts/MovPlatPensa.cs
+++ Assets/Scripts/MovPlatPensa.cs
@@ -6,49 +6,46 @@ using UnityEngine;
 
 public class MovPlatPensa : MonoBehaviour
 {
-    private float yPos, yAlto, yBaixo, t, timeStamp;
+    private float y0, y, yMax, yMin, t, timeStamp;
     private Boolean playerEncima;
     public float tolerancia, dist, k;
     void Start()
     {
         playerEncima = false;
-        yAlto = gameObject.transform.position.y + dist/2;
-        yBaixo = yAlto - dist;
-        timeStamp = 0;
+        timeStamp = Time.time;
+        y0 = gameObject.transform.position.y;
+        yMax = y0 + dist/2;
+        yMin = yMax - dist;
+        Debug.Log("alto = " + (yMax ) + "; baixo = " + (yMin ));
+        Debug.Log("alto = " + (yMax - tolerancia*dist) + "; baixo = " + (yMin + tolerancia*dist));
     }
 
     void Update()
     {
         t = Time.time - timeStamp;
-        yPos = gameObject.transform.position.y;
-        if(t < 5/k) {   // se não passou muito tempo...
-            if(playerEncima) {
-                // descer a plataforma
-                transform.position += Vector3.up * (yBaixo - yPos)*k*Mathf.Exp(-k*t)*Time.deltaTime;
-            } else {
-                // subir a plataforma
-                transform.position += Vector3.up * (yAlto - yPos)*k*Mathf.Exp(-k*t)*Time.deltaTime;
-            }
+        y = gameObject.transform.position.y;
+        if(playerEncima && y > yMin + tolerancia*dist) {
+            // descer a plataforma se estiver muito alta
+            transform.position += Vector3.up * (yMin - y0)*k*Mathf.Exp(-k*t)*Time.deltaTime;
+        } else if(!playerEncima && y < yMax - tolerancia*dist) {
+            // subir a plataforma se estiver muito baixa
+            transform.position += Vector3.up * (yMax - y0)*k*Mathf.Exp(-k*t)*Time.deltaTime;
         }
     }
 
-    void OnCollisionEnter2D(Collision2D other) {
-        if(other.gameObject.CompareTag("Player") && other.gameObject.transform.position.y > yPos + tolerancia) {
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.CompareTag("Player") && other.gameObject.transform.position.y > y0 + 0.5) {
             playerEncima = true;
             timeStamp = Time.time;
+            y0 = gameObject.transform.position.y;
         }
     }
 
-    void OnCollisionStay2D(Collision2D other) {
-        if(other.gameObject.CompareTag("Player") && other.gameObject.transform.position.y > yPos + tolerancia) {
-            playerEncima = true;
-        }
-    }
-
-    void OnCollisionExit2D(Collision2D other) {
+    void OnTriggerExit2D(Collider2D other) {
         if(other.gameObject.CompareTag("Player")) {
             playerEncima = false;
             timeStamp = Time.time;
+            y0 = gameObject.transform.position.y;
         }
     }
 }
