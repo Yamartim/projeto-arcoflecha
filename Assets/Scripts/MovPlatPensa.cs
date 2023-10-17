eff7e73a25a4b9deb42016df345ae3459859faca
pedmig <pedromiguel.sp17@gmail.com>
2023-10-16 23:15:51 -0300

    plataforma flutuante
    a lógica da colisão com o player precisa ser mudada

--- Assets/Scripts/MovPlatPensa.cs
+++ Assets/Scripts/MovPlatPensa.cs
@@ -6,22 +6,40 @@ using UnityEngine;
 
 public class MovPlatPensa : MonoBehaviour
 {
-    private float yPos;
+    private float yPos, yAlto, yBaixo, t, timeStamp;
     private Boolean playerEncima;
-    public float tolerancia;
+    public float tolerancia, dist, k;
     void Start()
     {
         playerEncima = false;
+        yAlto = gameObject.transform.position.y + dist/2;
+        yBaixo = yAlto - dist;
+        timeStamp = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        t = Time.time - timeStamp;
         yPos = gameObject.transform.position.y;
-        Debug.Log(playerEncima);
+        if(t < 5/k) {   // se não passou muito tempo...
+            if(playerEncima) {
+                // descer a plataforma
+                transform.position += Vector3.up * (yBaixo - yPos)*k*Mathf.Exp(-k*t)*Time.deltaTime;
+            } else {
+                // subir a plataforma
+                transform.position += Vector3.up * (yAlto - yPos)*k*Mathf.Exp(-k*t)*Time.deltaTime;
+            }
+        }
     }
 
     void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Player") && other.gameObject.transform.position.y > yPos + tolerancia) {
+            playerEncima = true;
+            timeStamp = Time.time;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D other) {
         if(other.gameObject.CompareTag("Player") && other.gameObject.transform.position.y > yPos + tolerancia) {
             playerEncima = true;
         }
@@ -30,6 +48,7 @@ public class MovPlatPensa : MonoBehaviour
     void OnCollisionExit2D(Collision2D other) {
         if(other.gameObject.CompareTag("Player")) {
             playerEncima = false;
+            timeStamp = Time.time;
         }
     }
 }
