f826c4763e7d847cbd505ff68ef82d1fce944d1a
yararodri <137322018+yararodri@users.noreply.github.com>
2023-11-01 12:28:45 -0300

    UI

--- Assets/Scripts/Diario.cs
+++ Assets/Scripts/Diario.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,7 +9,7 @@ public class Diario : MonoBehaviour
     [SerializeField] List<Transform> paginas;
     int index = -1;
     bool rotate = false;
-    [SerializeField] GameObject VoltarButton;
+    [SerializeField] GameObject AnteriorButton;
     [SerializeField] GameObject ProximoButton;
     
 
@@ -26,7 +26,7 @@ public class Diario : MonoBehaviour
             paginas[i].transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
         paginas[0].SetAsLastSibling();
-        VoltarButton.SetActive(false);
+        AnteriorButton.SetActive(false);
     }
 
     public void Proximo()
@@ -41,9 +41,9 @@ public class Diario : MonoBehaviour
 
     public void ProximoButtonActions()
     {
-        if (VoltarButton.activeInHierarchy == false)
+        if (AnteriorButton.activeInHierarchy == false)
         {
-            VoltarButton.SetActive(true);
+            AnteriorButton.SetActive(true);
         }
         if (index == paginas.Count - 1)
         {
@@ -51,16 +51,16 @@ public class Diario : MonoBehaviour
         }
     }
 
-    public void Voltar()
+    public void Anterior()
     {
         if (rotate == true) { return; }
         float angle = 0;
         paginas[index].SetAsLastSibling();
-        VoltarButtonActions();
+        AnteriorButtonActions();
         StartCoroutine(Rotate(angle, false));
     }
 
-    public void VoltarButtonActions()
+    public void AnteriorButtonActions()
     {
         if (ProximoButton.activeInHierarchy == false)
         {
@@ -68,7 +68,7 @@ public class Diario : MonoBehaviour
         }
         if (index - 1 == -1)
         {
-            VoltarButton.SetActive(false);
+            AnteriorButton.SetActive(false);
         }
     }
 
@@ -95,4 +95,118 @@ public class Diario : MonoBehaviour
         }
     }
 
+}*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Diario : MonoBehaviour
+{
+    [SerializeField] float pageSpeed = 0.1f;
+    [SerializeField] List<Transform> paginas;
+    int index = -1;
+    bool rotate = false;
+    [SerializeField] GameObject AnteriorButton;
+    [SerializeField] GameObject ProximoButton;
+    int paginasColetadas = 0; // Contagem de páginas coletadas
+
+    private void Start()
+    {
+        InitialState();
+        Time.timeScale = 1f;
+    }
+
+    public void InitialState()
+    {
+        for (int i = 0; i < paginas.Count; i++)
+        {
+            paginas[i].transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        paginas[0].SetAsLastSibling();
+        AnteriorButton.SetActive(false);
+    }
+
+    public void Proximo()
+    {
+        if (rotate == true || paginasColetadas <= index) { return; }
+        index++;
+        float angle = -180;
+        ProximoButtonActions();
+        paginas[index].SetAsLastSibling();
+        StartCoroutine(Rotate(angle, true));
+    }
+
+    public void ProximoButtonActions()
+    {
+        if (AnteriorButton.activeInHierarchy == false)
+        {
+            AnteriorButton.SetActive(true);
+        }
+        if (index == paginas.Count - 1 || index >= paginasColetadas)
+        {
+            ProximoButton.SetActive(false);
+        }
+    }
+
+    public void Anterior()
+    {
+        if (rotate == true || index <= 0) { return; }
+        float angle = 0;
+        paginas[index].SetAsLastSibling();
+        AnteriorButtonActions();
+        StartCoroutine(Rotate(angle, false));
+    }
+
+    public void AnteriorButtonActions()
+    {
+        if (ProximoButton.activeInHierarchy == false)
+        {
+            ProximoButton.SetActive(true);
+        }
+        if (index <= 0)
+        {
+            AnteriorButton.SetActive(false);
+        }
+    }
+
+    IEnumerator Rotate(float angle, bool Proximo)
+    {
+        float value = 0f;
+        while (true)
+        {
+            rotate = true;
+            Quaternion targetRotation = Quaternion.Euler(0, angle, 0);
+            value += Time.deltaTime * pageSpeed;
+            paginas[index].localRotation = Quaternion.Slerp(paginas[index].localRotation, targetRotation, value);
+            float angle1 = Quaternion.Angle(paginas[index].localRotation, targetRotation);
+            if (angle1 < 0.1f)
+            {
+                if (Proximo == false)
+                {
+                    index--;
+                }
+                rotate = false;
+                break;
+            }
+            yield return null;
+        }
+    }
+
+    public void ColetarPagina()
+    {
+        if (paginasColetadas < paginas.Count)
+        {
+            paginasColetadas++;
+            Debug.Log("Página coletada. Total de páginas coletadas: " + paginasColetadas);
+            ProximoButton.SetActive(true);
+        }
+
+        if (paginasColetadas < paginas.Count)
+        {
+            paginasColetadas++;
+            ProximoButton.SetActive(true);
+        }
+    }
 }
