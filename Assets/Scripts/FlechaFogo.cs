2347388b95b8cc060a896a17ff422439b8c4d25e
Yamartim <54243954+Yamartim@users.noreply.github.com>
2023-11-19 12:33:28 -0300

    ajustes em prefabs e assets

--- Assets/Scripts/FlechaFogo.cs
+++ Assets/Scripts/FlechaFogo.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class FlechaFogo : Flecha
 {
+        [SerializeField] GameObject aguaPreFab;
+
     private void OnCollisionEnter2D(Collision2D other){
         if(other.gameObject.CompareTag("Madeira")){   
             Destroy(other.gameObject);
+        }
+
+        if(other.gameObject.CompareTag("Gelo")){   
+            Vector2 position = other.gameObject.transform.position;
+            Quaternion rotation = other.gameObject.transform.rotation;
+            
+            aguaPreFab.transform.localScale = other.gameObject.transform.localScale;
+            
+            Destroy(other.gameObject);
+            Instantiate(aguaPreFab, position, rotation);
         }
     }
 }
