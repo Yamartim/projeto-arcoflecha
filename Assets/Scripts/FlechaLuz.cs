1cc53107ad5cf52c1f5cd6cf87cc6da6940c4f2c
João Vitor Simplicio <joaovitorvs0309@gmail.com>
2023-11-20 16:17:53 -0300

    Bug da corrupção
    agora a flecha de luz apaga um bloco por vez

--- Assets/Scripts/FlechaLuz.cs
+++ Assets/Scripts/FlechaLuz.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FlechaLuz : Flecha
 {
-    private void OnCollisionEnter2D(Collision2D other){
+    private void OnTriggerStay2D(Collider2D other){
         if(other.gameObject.CompareTag("Corrupcao")){   
             Destroy(other.gameObject);
         }
