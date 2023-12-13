1e98ad44a9bd123ab54a9c49e503e65d6a65640f
pedmig <pedromiguel.sp17@gmail.com>
2023-12-12 21:17:35 -0300

    corrupcao animada (menos os blocos do cassati)

--- Assets/Scripts/Animacao_Corrupcao.cs
+++ Assets/Scripts/Animacao_Corrupcao.cs
@@ -9,13 +9,11 @@ public class Animacao_Corrupcao : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // rotaciona alguns blocos de corrupção aleatoriamente por frame
-        for(int i=1; i <= 2; i++) {
-            int indGrupo = Random.Range(0, gameObject.transform.childCount);
-            Transform grupo = gameObject.transform.GetChild(indGrupo);
-            int indAleatorio = Random.Range(0, grupo.transform.childCount);
-            Transform blocoAleatorio = grupo.transform.GetChild(indAleatorio);
-            blocoAleatorio.transform.Rotate(0.0f, 0.0f, 90.0f);
+        // dar 1 chance em 100 de rotacionar num frame
+        int numAleatorio = Random.Range(0, 100);
+        if(numAleatorio == 50 && gameObject.GetComponent<Renderer>().isVisible) {
+            // apenas rotaciona se estiver visível
+            gameObject.transform.Rotate(0.0f, 0.0f, 90.0f);
         }
     }
 }
