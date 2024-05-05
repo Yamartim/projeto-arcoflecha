d57fee8f462bfc4068f70909425333b7a6a9379a
Yamartim <54243954+Yamartim@users.noreply.github.com>
2024-05-05 19:17:45 -0300

    hotfix nome

--- Assets/Scripts/AnimCorrupcao.cs
+++ Assets/Scripts/AnimCorrupcao.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Animacao_Corrupcao : MonoBehaviour
+public class AnimCorrupcao : MonoBehaviour
 {
     // Start is called before the first frame update
 
