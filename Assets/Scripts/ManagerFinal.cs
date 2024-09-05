efe0782b4b8f9ec9edab9c91d5e584b4e93fb900
Yamartim <54243954+Yamartim@users.noreply.github.com>
2024-09-05 09:37:55 -0300

    começo de trabalho na cutscene final

--- Assets/Scripts/ManagerFinal.cs
+++ Assets/Scripts/ManagerFinal.cs
@@ -1,25 +1,63 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ManagerFinal : MonoBehaviour
 {
     [SerializeField] float duracaoFinal = 15f;
+    [SerializeField] float tempoEscolha = 20f;
+    bool timerAtivo = false;
+    float tempo;
+
     Animator anim;
     //Collider2D coll;
+
+    [SerializeField] Slider timerSlider;
+    [SerializeField] ImagemFinalSelector imgSel;
+    [SerializeField] PlayableDirector cutscene;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         anim = transform.parent.GetComponentInChildren<Animator>();
         //coll = GetComponentInChildren<Collider2D>();
+
+        timerSlider.maxValue = tempoEscolha;
+        timerSlider.value = tempoEscolha;
+        tempo = tempoEscolha;
+    }
+
+    void Update()
+    {
+        if(timerAtivo)
+        {
+            tempo -= Time.deltaTime;
+        }
+
+        if(tempo <= 0f)
+        {
+            timerAtivo = false;
+            imgSel.SetImagemFinal(2);
+            anim.SetTrigger("FINAL");
+            StartCoroutine(MudarCena());
+
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.CompareTag("Player"))
         {
             other.GetComponent<PlayerMovimento>().ToggleMovimento(false);
-            anim.SetTrigger("FINAL");
-            StartCoroutine(MudarCena());
+            //anim.SetTrigger("FINAL");
+            //StartCoroutine(MudarCena());
+
+            //COMEÇAR CUTSCENE AQ
+            //NO FINAL DELA MOSTRAR A ESCOLHA E LIGAR O TIMER
+            cutscene.Play();
 
         }
     }
@@ -36,4 +74,10 @@ public class ManagerFinal : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(transform.position, transform.localScale);
     }
+
+    public void ToggleTimer(bool toggle)
+    {
+        timerAtivo = toggle;
+    }
+
 }
